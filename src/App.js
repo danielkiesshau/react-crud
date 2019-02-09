c7aa89511bf7732d2b0f2c2d7f0f9134b85783f7
Daniel Kiesshau <danielkiesshau@outlook.com>
2019-02-09 21:08:09 -0200

    adding todos

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      newTodo: '',
       todos: [
         {
           id: 1,
@@ -25,7 +26,9 @@ class App extends Component {
         },
       
       ]
-    }
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
   }
   render() {
     return (
@@ -46,16 +49,48 @@ class App extends Component {
         </header>
         <div className="container">
           <h2 className="text-center p-4">Todos App</h2>
+          <input
+            type="text"
+            name="todo"
+            className="my-4 form-control"
+            onChange={this.handleChange}
+            value={this.state.newTodo}
+          />
+          <button
+            className="btn-info mb-3 form-control"
+            onClick={this.addTodo}
+          >
+            Add Todo
+          </button>
           <ul className="list-group">
             {
-              this.state.todos.map(({ name }) => (
-                <li className="list-group-item">{name}</li>
+              this.state.todos.map(({ id, name }) => (
+                <li
+                  key={id}
+                  className="list-group-item"
+                >
+                  {name}
+                </li>
               ))
             }
           </ul>
         </div>
       </div>
     );
+  }
+
+  handleChange(event) {
+    this.setState({ newTodo: event.target.value });
+  }
+
+  addTodo() {
+    const { todos, newTodo } = this.state;
+    const id = todos.length - 1;
+    const objTodo = {
+      id,
+      name: newTodo
+    };
+    this.setState({ ...this.state, todos: [...todos,  objTodo]});
   }
 }
 
