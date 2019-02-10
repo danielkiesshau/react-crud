5b5871a99d725b53898a377d49f68a543a2cc5b8
Daniel Kiesshau <danielkiesshau@outlook.com>
2019-02-09 22:11:59 -0200

    component List moved to components folder

---
+++ src/components/List.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.editTodo = this.editTodo.bind(this);
+  }
+  render () {
+    const { todos, editing } = this.props;
+    if (editing) return null;
+    const todosElements = todos.map(({ id, name }, index) => (
+      <li
+        key={id}
+        className="list-group-item"
+      >
+        <button
+          className="btn-sm mr-4 btn btn-warning"
+          onClick={(ev) => this.editTodo(index)}
+        >
+          U
+        </button>
+        {name}
+        <button
+          className="btn-sm ml-4 btn btn-danger"
+          onClick={(ev) => this.deleteTodo(index)}
+        >
+          X
+        </button>
+      </li>
+    ));
+
+    return todosElements;
+  }
+
+  deleteTodo(index) {
+    const { todos } = this.props;
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    this.props.setState({ ...this.props, todos: newTodos, newTodo: '', editing: false });
+  }
+  
+  editTodo(index) {
+    const todo = this.props.todos[index];
+    this.props.setState({
+      editing: true,
+      newTodo: todo.name,
+      editingIndex: index,
+    });
+  }
+}
